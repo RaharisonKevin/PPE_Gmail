74de850ab6ae8845d9262c81298f6aa079b74408
RaharisonKevin <rkevinandy@yahoo.fr>
2021-03-15 12:15:27 +0100

    first commit

---
+++ run.bat
@@ -0,0 +1,8 @@
+cd C:\Users\Kevin\Desktop\git\PPE_Gmail
+git init
+git add *
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/RaharisonKevin/PPE_Gmail.git
+git push -u origin master
+pause
