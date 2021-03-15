0565d60b1644ee306b54ebb1b9b862d68746167f
RaharisonKevin <rkevinandy@yahoo.fr>
2021-03-15 12:31:36 +0100

    first commit

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', (e)=>{
     e.preventDefault()
      console.log("loaded")
       const linkList = document.querySelectorAll("header nav ul:nth-child(2) li a");
-      const submitBtn = document.getElementsByTagName("input")[5]
+      const submitBtn = document.getElementsByTagName("input")[4]
   
       //create virtual element warning 
   
@@ -11,10 +11,10 @@ document.addEventListener('DOMContentLoaded', (e)=>{
       parent.prepend(element)
   
       const User = ({
-        firstname: document.getElementsByTagName("input")[1],
-        lastname: document.getElementsByTagName("input")[2],
-        email: document.getElementsByTagName("input")[3],
-        password: document.getElementsByTagName("input")[4],
+        firstname: document.getElementsByTagName("input")[0],
+        lastname: document.getElementsByTagName("input")[1],
+        email: document.getElementsByTagName("input")[2],
+        password: document.getElementsByTagName("input")[3],
   
             userDataControll(){
               if(!this.firstname.value || !this.lastname.value || !this.email.value || !this.password.value){
